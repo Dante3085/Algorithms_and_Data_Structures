28a20f03e48c4668dc64ffbb00ce237ff7bc97f4
mjsch <moritz97.s@web.de>
2020-08-02 15:30:32 +0200

    Implemented BubbleSort

--- Sorting.h
+++ Sorting.h
@@ -23,4 +23,30 @@ namespace Sorting
 			v[j + 1] = value;
 		}
 	}
+
+	void swap(std::vector<int>& v, int i, int j)
+	{
+		int temp = v[i];
+		v[i] = v[j];
+		v[j] = temp;
+	}
+
+	void bubbleSort(std::vector<int>& v)
+	{
+		if (v.size() <= 1)
+			return;
+
+		int firstSortedIndex = v.size();
+		for (int i = 0; i < v.size() - 1; ++i)
+		{
+			int a = 0;
+			int b = 1;
+			for (; b < firstSortedIndex; ++a, ++b)
+			{
+				if (v[a] > v[b])
+					swap(v, a, b);
+			}
+			--firstSortedIndex;
+		}
+	}
 }
